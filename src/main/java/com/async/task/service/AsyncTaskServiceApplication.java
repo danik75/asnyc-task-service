4113608e6ed31bbf1397f00f48c0f5c1c47efb16
dani <dani.pritsker@gmail.com>
2024-02-11 11:25:57 +0200

    initial spring boot commit

---
+++ src/main/java/com/async/task/service/AsyncTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.async.task.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/async/task/service/AsyncTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.async.task.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
